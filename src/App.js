30289d194bacaeeb08e5dc6e0a7d6255ef4c7c73
ashish-a dubey <ashish-a.dubey@cplcloud.com>
2024-01-13 13:30:58 +0530

    React Setup using Webapck and Babel

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const App = () => {
+    return <h1>Create React Project using webpack and babel</h1>;
+};
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+import './App.css';
+
+const el = document.getElementById('app');
+
+ReactDOM.render(<App />, el);
